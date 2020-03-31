2eafb85863c9917f64866e55b0b70f1a8e387023
DUMAS <you@example.com>
2020-03-31 21:37:19 +0200

    GDU Menu V 1.1

--- src/app/gdu-menu/gdu-menu.component.ts
+++ src/app/gdu-menu/gdu-menu.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
 
-
-
 @Component({
   selector: 'app-gdu-menu',
   templateUrl: './gdu-menu.component.html',
