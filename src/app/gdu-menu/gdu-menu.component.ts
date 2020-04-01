7668afae1bcbe3858ba7a42602350352dd64d3aa
DUMAS <you@example.com>
2020-04-01 09:39:03 +0200

    GDU Master actualisation Menu V 1.0

--- src/app/gdu-menu/gdu-menu.component.ts
+++ src/app/gdu-menu/gdu-menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import { FormGroup, FormControl, Validators} from '@angular/forms';
+
 
 
 @Component({
@@ -9,10 +9,21 @@ import { FormGroup, FormControl, Validators} from '@angular/forms';
   styleUrls: []
 })
 export class GduMenuComponent implements OnInit {
+  validatingForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.validatingForm = new FormGroup({
+      loginFormModalEmail: new FormControl('', Validators.email),
+      loginFormModalPassword: new FormControl('', Validators.required)
+    });
   }
-
+  get loginFormModalEmail() {
+    return this.validatingForm.get('loginFormModalEmail');
+  }
+  get loginFormModalPassword() {
+    return this.validatingForm.get('loginFormModalPassword');
+  }
 }
+
