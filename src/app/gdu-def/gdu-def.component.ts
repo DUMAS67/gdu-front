7370b33b7eb0f9f9a544fc33ce1adecb340299f7
DUMAS <you@example.com>
2020-04-03 12:20:48 +0200

    GDU REG DOC V 1.0

---
+++ src/app/gdu-def/gdu-def.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gdu-def',
+  templateUrl: './gdu-def.component.html',
+  styleUrls: []
+})
+export class GduDefComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
