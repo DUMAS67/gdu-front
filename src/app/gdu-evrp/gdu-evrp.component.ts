cc428a44c55d304350a69536b2cb79b8b57349ab
DUMAS <you@example.com>
2020-04-03 15:23:10 +0200

    GDU EvRP Modif UT V 1.2

--- src/app/gdu-evrp/gdu-evrp.component.ts
+++ src/app/gdu-evrp/gdu-evrp.component.ts
@@ -10,12 +10,33 @@ import { Router } from '@angular/router';
 export class GduEvrpComponent implements OnInit {
 
   constructor(private _router: Router) { }
-
-  ngOnInit() {
+recup: any;
+  elements3: any = [{
+    id: '1',
+    first: 'Unité de Travail1'
+  }, { id: '2', first: 'Unité de Travail2' }, {
+    id: '3', first: 'Unité de Travail3'
+  }, {
+    id: '4',first: 'Unité de Travail4'
+  }, {
+    id: '5', first: 'Unité de Travail5'
+  }, {    id: '6', first: 'Unité de Travail6'
   }
 
+  ];
+  headElements3 = ['ID', 'UT', 'Sélection'];
+
+  ngOnInit() {
+
+  }
   choixSortir() {
 
     this._router.navigate(['/gdu/deconnexion']);
+  }
+
+  recupUt(utValeur: any[]) {
+
+this.recup = utValeur;
+console.log(this.recup);
   }
 }
